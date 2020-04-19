b140452747679dea0388982b32e5511242a7d565
shuvasisdatta <shuvasisdatta@gmail.com>
2020-04-19 21:48:20 +0600

    git push changed to shuvasisdatta.github.io

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,6 @@
 set -e
 
 # build
-# npm run build
 yarn lint
 yarn build
 
@@ -19,9 +18,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-#git push -f git@github.com:shuvasisdatta/shuvasisdatta.github.io.git master
+git push -f git@github.com:shuvasisdatta/shuvasisdatta.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:shuvasisdatta/portfolio.git master:gh-pages
+#git push -f git@github.com:shuvasisdatta/portfolio.git master:gh-pages
 
 cd -
