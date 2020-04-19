b4f1ccbc23ea502668f91ab7dc15cc4dec2583b6
shuvasisdatta <shuvasisdatta@gmail.com>
2020-04-19 21:15:19 +0600

    replace npm with yarn and git push repo changed

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ yarn build
 # navigate into the build output directory
 cd dist
 
-# if you are deploying to a custom domain
+# if you are deploying to a custom domain echo '<YOURDOMAINNAME>' > CNAME
 echo 'www.shuvasisdatta.com' > CNAME
 
 git init
@@ -19,9 +19,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:shuvasisdatta/shuvasisdatta.github.io.git master
+#git push -f git@github.com:shuvasisdatta/shuvasisdatta.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:shuvasisdatta/portfolio.git master:gh-pages
 
 cd -
