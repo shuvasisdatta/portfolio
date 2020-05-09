9fc21eade126a56b63f64ac9336d052904c2b895
Shuvasis Datta <shuvasisdatta@gmail.com>
2020-05-09 18:49:27 +0600

    removed duplicate about article

--- src/views/Research.vue
+++ src/views/Research.vue
@@ -2,24 +2,8 @@
   <section class="section about">
     <div class="container">
       <h2 class="title is-uppercase">Research</h2>
-      <article class="has-text-justified is-italic">
-        Hi, I am Shuvasis Datta graduated in Electrical & Electronic Engineering
-        from Chittagong University of Engineering & Technology in March 2017. I
-        enjoyed learning and working in Robo Mechatronics Association,
-        Bangladesh as IT secretary in the whole period of my undergraduate
-        university. I believe in sharing knowledge that's why I am an open
-        source enthusiast, all of my work is open sourced and always will be. I
-        have a keen interest in Robotics, Embedded Systems, Automation and
-        Mathematics. Besides, I love to read books, online technical and
-        non-technical articles. In short, I enjoy coding, traveling, watching
-        movies/tv-series in my spare time.
-      </article>
       <div class="columns" style="margin-top: 50px;">
-        <div
-          class="column"
-          v-for="research in researches"
-          :key="research.division"
-        >
+        <div class="column" v-for="research in researches" :key="research.division">
           <span class="fa-stack fa-4x has-text-black-bis">
             <fa-layers class="fa-2x">
               <!-- <fa-icon :icon="['fas', 'circle', 'fa-stack-2x']" /> -->
@@ -47,16 +31,14 @@
             class="has-text-white"
             :href="pub.url"
             target="_blank"
-            >{{ pub.title }}</a
-          >
+          >{{ pub.title }}</a>
         </div>
         <div class="column is-one-fifths has-text-right-desktop">
           <div class="tags has-addons are-medium">
             <span
               class="tag is-capitalized"
               :class="pub.status == 'published' ? 'is-primary' : 'is-warning'"
-              >{{ pub.status }}</span
-            >
+            >{{ pub.status }}</span>
             <span class="tag is-dark">{{ pub.published_year }}</span>
           </div>
         </div>
