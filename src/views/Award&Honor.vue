73e12f5eb2df0a29f04e165433582da9b485395f
Shuvasis Datta <shuvasisdatta@gmail.com>
2021-06-11 18:41:34 +0600

    Award - Robo Nurse Added

--- src/views/Award&Honor.vue
+++ src/views/Award&Honor.vue
@@ -10,24 +10,49 @@
         </div>
         <div class="column has-text-justified">
           <article>
-            Our Team 'RMA TESLA' is one of the top 3 teams selected for funding
-            BDT 500k for innovative real problem-solving categories in the
-            Microsoft-Young Bangla Award 2017. The Check is received from Sajeeb
-            Wazed Joy, Honourable Advisor to the Government of Bangladesh on
-            Information and Communication Technology. This opportunity is given
-            by Microsoft Bangladesh as their Intern program.
+            Team ’RMA Tesla’ has been funded by the Microsoft Bangladesh and
+            Young Bangla jointly for startup based on the project titled "Pathak
+            - A Reading Device for Visually Handicapped Persons Using Image
+            Processing". The Project was awarded for innovative real
+            problem-solving categories in the Microsoft-Young Bangla Award 2017.
+            The Check is received from Sajeeb Wazed Joy, Honourable Advisor to
+            the Government of Bangladesh on Information and Communication
+            Technology.
             <br />
-            <br />About Our Project - পাঠক (Patha)
+            <br />About Our Project - পাঠক (Pathak)
             <hr />
-            We built a Electronic Reading Device for Visually Handicapped/ Blind
-            People to read out the text- books, magazines written in bengali so
-            that they can easily read & get rid of old technology Braile .
+            We built a Electronic Reading Device for Visually Impaired people to
+            read out the text- books, magazines written in bengali so that they
+            can easily read & get rid of old Braile technology.
             <br />
             <br />Team Members
             <hr />
             <li>Shuvasis Datta (Team Leader)</li>
             <li>Mahade Sarkar (Challenger)</li>
             <li>Ridwan Samir Tanim (Gate Keeper)</li>
+          </article>
+        </div>
+      </div>
+      <br />
+      <div class="columns">
+        <div class="column">
+          <figure class="image is-5by4">
+            <img src="@/assets/img/awards/Robo_Nurse.jpeg" />
+          </figure>
+        </div>
+        <div class="column has-text-justified">
+          <article>
+            Project "Robo Nurse" has been praised in the hackathon titled
+            "Make-A-Thon" organised by the World Bank and BetterStories Limited
+            in partnership with the ICT Division of Bangladesh in 2016.
+            <br />
+            <br />About Our Project - Robo Nurse
+            <hr />
+            A Robot that provide basic healthcare services to a patient. It can
+            measure ECG, temperature and other factors which are send
+            automatically to a family doctor via sms or internet. It also
+            provide medication in proper time. The prototype was planned and
+            made within 68 hours in the hackathon.
           </article>
         </div>
       </div>
