05cb159e6c8af96c69a8930cb295b235897c3311
shuvasisdatta <shuvasisdatta@gmail.com>
2021-07-29 14:19:38 +0600

    update bio

--- src/views/About.vue
+++ src/views/About.vue
@@ -39,20 +39,7 @@
         <div class="column">
           <div class="column">
             <article class="has-text-justified">
-              Hi, I am Shuvasis Datta graduated in Electrical & Electronic
-              Engineering from Chittagong University of Engineering & Technology
-              in March 2017. I enjoyed learning and working in Robo Mechatronics
-              Association, Bangladesh as IT secretary during my undergraduate. I
-              believe in sharing knowledge that's why I am an open source
-              minded. I am skilled in DCS (Emerson DeltaV S & M Series, ABB
-              800xA), PLC (Allen-Bradley, Siemens), SCADA(Indusoft Web Studio,
-              HMI (PanelView), Embedded Systems (Microcontroller, Arduino, ESP,
-              Raspberry Pi), Robotics, AutoCAD, MATLAB, IoT, and Full Stack App
-              Development. I have keen interest in Machine Learning, Image
-              Processing, Embedded Systems, Robotics, Automation, and
-              Mathematics. Besides, I love to read books, online technical and
-              non-technical articles. In short, I enjoy coding, watching movies
-              and tv-series in my spare time.
+              Hi, Shuvasis Datta graduated from Chittagong University of Engineering & Technology in March 2017 with a bachelor's degree in Electrical & Electronic Engineering. I enjoyed learning and working in Robo Mechatronics Association as an IT secretary during my undergraduate. I believe in information sharing, which is why I am open-source-oriented. I am skilled in DCS (Emerson DeltaV S & M Series, ABB 800xA), PLC (Allen-Bradley, Siemens), SCADA(Indusoft Web Studio, HMI (PanelView), Embedded Systems (Microcontroller, Arduino, ESP, Raspberry Pi), Robotics, AutoCAD, MATLAB, IoT, and Full Stack App Development. I have a keen interest in Machine Learning, Image Processing, Embedded Systems, Robotics, Automation, and Mathematics. Besides, I love to read books, online technical and non-technical articles. In brief, I enjoy coding, watching movies, and tv-series in my leisure time.
             </article>
           </div>
           <div class="column has-text-left-desktop">
